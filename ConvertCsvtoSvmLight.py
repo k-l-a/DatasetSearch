e5edc326b70559e81f6aa1a533739276759461a6
kla <k.l.a@u.nus.edu>
2019-12-23 02:05:33 +0800

    Update version

--- ConvertCsvtoSvmLight.py
+++ ConvertCsvtoSvmLight.py
@@ -12,12 +12,20 @@ from collections import defaultdict
 
 def construct_line( label, line ):
 	new_line = []
-	if float( label ) == 0.0:
+	try:
+		if float( label ) == 0.0:
+			label = "0"
+	except ValueError:
 		label = "0"
+
 	new_line.append( label )
 
 	for i, item in enumerate( line ):
-		if item == '' or float( item ) == 0.0:
+		try:
+			if item == '' or float(item) == 0.0:
+				continue
+		except ValueError:
+			#Skip categorical
 			continue
 		new_item = "%s:%s" % ( i + 1, item )
 		new_line.append( new_item )
@@ -27,32 +35,37 @@ def construct_line( label, line ):
 
 # ---
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+def convert(input, output, label_index, skip_header):
+	input_file = input
+	output_file = output
 
-try:
-	label_index = int( sys.argv[3] )
-except IndexError:
-	label_index = 0
+	try:
+		label_index = int( label_index )
+	except IndexError:
+		label_index = 0
 
-try:
-	skip_headers = sys.argv[4]
-except IndexError:
-	skip_headers = 0
+	try:
+		skip_headers = skip_header
+	except IndexError:
+		skip_headers = 0
 
-i = open( input_file, 'rt' )
-o = open( output_file, 'wt' )
+	i = open( input_file, 'rt' )
+	o = open( output_file, 'wt' )
 
-reader = csv.reader(i)
+	reader = csv.reader(i)
 
-if skip_headers:
-	headers = next(reader, None)
+	if skip_headers:
+		headers = next(reader, None)
 
-for line in reader:
-	if label_index == -1:
-		label = '1'
-	else:
-		label = line.pop( label_index )
+	for line in reader:
+		if label_index == -1:
+			label = '1'
+		else:
+			label = line.pop( label_index )
 
-	new_line = construct_line( label, line )
-	o.write( new_line )
+		try:
+			new_line = construct_line( label, line )
+			o.write( new_line )
+		except ValueError:
+			#do nothing
+			new_line = ""
--- Search.py
+++ Search.py
@@ -1,34 +1,148 @@
 import numpy as np
+import autosklearn.classification
+import warnings
 import faiss
+from MetaFeatures.Output import MetaFeatures
 from MetaFeatures import Output
+from dataset2vec import main as d2v
+from dataset2vec import config
+from enum import Enum
+import ConvertCsvtoSvmLight
+import math
 import os
+import pandas as pd
+import pickle
+
+warnings.filterwarnings("ignore", "Mean of empty slice")
 
 path = ""
-defaultPath = "./ConvertedDatasets/"
+defaultPath = "./SavedDatasets/"
+savePath = "./SavedDatasets/"
+exitCommand = "exit"
+loadCommand = "load"
+saveCommand = "save"
+indexFilename = "./saved_index2"
+fileListName = "./saved_filelist2"
+indexType = "L2"
 fileList = []
 searchTerm = ""
+weights = []
 
 useDefault = False
+useCSV = True
+used2v = False
+fromCsv = False
+isWeighted = False
+
+datasetProcessD2C = False
+
+
 d = 50
 k = 5
+
+
+class MeasureType(Enum):
+    L2 = 1
+    COS = 2
+    MANH = 3
+    MINK = 4
+
 
 def readData():
     folderpath = input("Path to dataset base folder :")
     global path
-    if useDefault:
+    if folderpath == loadCommand:
+        path = loadCommand
+    elif useDefault:
         path = defaultPath
     else:
         path = folderpath
 
+def separate(df):
+    n = len(df.columns)
+    df.columns = list(range(n))
+    x = df[list(range(2, n))]
+    y = df[1].astype('category')
+
+    return x, y
+
+def catToNum(df):
+    df.columns = list(range(len(df.columns)))
+    is_number = np.vectorize(lambda x: np.issubdtype(x, np.number))
+    arr = is_number(df.dtypes)
+    for i in range(len(arr)):
+        isNum = arr[i]
+        if not isNum:
+            df[i] = df[i].astype('category').cat.codes
+
+    return df
 def getMetafeatures():
     featureList = []
     global d
     for filename in os.listdir(path):
-        print(filename)
-        metaFeatures = normalizeDimension(Output.get_metafeatures(path + filename))
-        featureList.append(metaFeatures)
-        fileList.append(filename)
-    return np.array(featureList).astype('float32')
+        try:
+            print(filename)
+            actualPath = path
+            if useCSV:
+                x, y = separate(catToNum(pd.read_csv((path + filename))).fillna(0))
+            elif fromCsv:
+                ConvertCsvtoSvmLight.convert((path + filename), (savePath + filename), 1, 1)
+                actualPath = savePath
+
+            if useCSV:
+                metaFeatures = MetaFeatures(x, y).calculate()
+            else:
+                metaFeatures = Output.get_metafeatures(actualPath + filename)
+
+            if used2v:
+                d2vFeatures = d2v.main(config=config, dataset=path + filename, seed=0)
+                for i in d2vFeatures:
+                    metaFeatures.append(i)
+
+            d = len(metaFeatures)
+            print(d)
+            featureList.append(metaFeatures)
+            fileList.append(filename)
+        except Exception as e:
+            print("Could not get metafeatures of " + filename)
+            continue
+
+    arr = np.array(featureList).astype('float32')
+    print(arr.shape)
+    return arr
+
+
+# Function to process dataset. Accepts a dataset (x, y) and returns a 'dataset'(x', y') that has been processed
+def processDataset(x, y, model):
+    if datasetProcessD2C:
+        # get metafeatures
+        metafeatures = MetaFeatures(x, y).calculate()
+
+        # Train gdbt model and get hyperparams
+        # TBD : Use dataset2config
+        model = autosklearn.classification.AutoSklearnClassifier(include_estimators=["gradient_boosting", ],
+                                                                 exclude_estimators=None, ensemble_size=1)
+        model.fit(x, y)
+        hyperparams = model.get_params()
+        return metafeatures, hyperparams
+    else:
+        # Default: no processing done
+        return x, y
+
+
+def similarityModel(x, y, model, load=True):
+    if datasetProcessD2C:
+        model.fit(x, y)
+        return model
+    else:
+        # Default: create Faiss index
+        if load:
+            model = loadIndex(indexFilename)
+        else:
+            model = faiss.IndexFlatL2(d)
+        return model
+
+
 
 def normalizeDimension(vector):
     if len(vector) < d:
@@ -38,29 +152,99 @@ def normalizeDimension(vector):
         vector = vector[:d]
     return vector
 
+
+def normalizeMeta(vector, weights):
+    if len(vector) != len(weights):
+        print("Different dimension of vector and weights")
+    else:
+        for i in range(len(vector)):
+            vector[i] = vector[i] * math.sqrt(weights[i])
+
+
+def loadWeights(inputWeights):
+    if len(inputWeights) != d:
+        print("Error, expected " + d + " values, received " + len(inputWeights) + " values.")
+    else:
+        weights = inputWeights
+        isWeighted = True
+
+
+def loadIndex(filename):
+    return faiss.read_index(filename)
+
+
 def readSearchTerm():
     term = input("Dataset to use as search term :")
     global searchTerm
-    searchTerm = term
+    if term == "exit":
+        searchTerm = "exit"
+    elif useDefault:
+        searchTerm = defaultPath + term
+    else:
+        searchTerm = term
 
-def search(termPath, metaFeatureList):
+
+def search(termPath, metaFeatureList, load=True):
     global d, k
-    index = faiss.IndexFlatL2(d)
+    if load:
+        index = loadIndex(indexFilename)
+    else:
+        index = faiss.IndexFlatL2(d)
+        index.add(metaFeatureList)
     print(index.is_trained)
-    index.add(metaFeatureList)
-    termFeatures = normalizeDimension(Output.get_metafeatures(termPath))
-    termFeatures = np.array([termFeatures]).astype('float32')
-    D, I = index.search(termFeatures, k)
-    print(I[:5])
-    for i in I[:5]:
-        for j in i:
-            print(fileList[j])
+    if termPath == saveCommand:
+        faiss.write_index(index, indexFilename)
+        with open(fileListName, "wb") as fp:
+            pickle.dump(fileList, fp)
+        print("Index and file list saved")
+    else:
+        if useCSV:
+            x, y = separate(catToNum(pd.read_csv((termPath))).fillna(0))
+            termFeatures = MetaFeatures(x, y).calculate()
+        else:
+            termFeatures = Output.get_metafeatures(termPath)
+        termFeatures = np.array([termFeatures]).astype('float32')
+        D, I = index.search(termFeatures, k)
+        print(I[:5])
+        for i in I[:5]:
+            for j in i:
+                print(fileList[j])
+
+
+def readDatabaseInput():
+    isRead = True
+    while isRead:
+        try:
+            readData()
+            if path == loadCommand:
+                meta = []
+                with open(fileListName, "rb") as fp:
+                    global fileList
+                    fileList = pickle.load(fp)
+            else:
+                meta = getMetafeatures()
+            isRead = False
+        except Exception as e:
+            print(e)
+    return meta
+
+
+def readSearchInput():
+    try:
+        readSearchTerm()
+        if searchTerm == "exit":
+            return False
+        return True
+    except Exception as e:
+        print(e)
+
 
 def main():
-    readData()
-    meta = getMetafeatures()
-    readSearchTerm()
-    search(searchTerm, meta)
-
+    meta = readDatabaseInput()
+    while(readSearchInput()):
+        try:
+            search(searchTerm, meta, path == loadCommand)
+        except Exception as e:
+            print(e)
 
 main()
